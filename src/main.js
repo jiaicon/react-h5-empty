8ec6cca5e2f2e71783168a8cdc78b9fb78e47df1
whq361 <whq361@qq.com>
2017-09-23 17:55:51 +0800

    tabbar css

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ const container = document.getElementById('container');
 function renderComponent(component) {
   ReactDOM.render(
     <Provider store={store}>
-      <div>
+      <div className="container-data">
         {component}
         <Alert
           stack={{
