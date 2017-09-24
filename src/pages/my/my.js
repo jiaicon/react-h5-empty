eabe9969a4028ace6288d35a59e6316aa0499d37
whq361 <whq361@qq.com>
2017-09-25 00:40:00 +0800

    refresh

--- src/pages/my/my.js
+++ src/pages/my/my.js
@@ -40,13 +40,13 @@ class MyPage extends React.Component {
         <div>头像,昵称,志愿口号</div>
         <div>团队，项目，时长</div>
         <div>
-          <ul className="">
+          <ul className="page-item-container">
             <li><Link to="/">个人资料</Link></li>
             <li><Link to="/signin">我的证书</Link></li>
             <li><Link to="/my">我的家庭</Link></li>
             <li><Link to="/">我的收藏</Link></li>
             <li><Link to="/signin">服务中心</Link></li>
-            <li><Link to="/my">设置(修改密码,退出登录)</Link></li>
+            <li><Link to="/my/setting">设置(修改密码,退出登录)</Link></li>
           </ul>
         </div>
 
