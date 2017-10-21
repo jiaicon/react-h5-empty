2f013012f622b4c43c4d39f3bdb1050157161ff9
chenminliang01 <chenminliang01@baidu.com>
2017-10-21 12:25:40 +0800

    样式调整

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -69,7 +69,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.ff8d91f5836fd120fd94.js"></script>
+  <script src="/dist/main.c509682eca4bb7caabb7.js"></script>
 </body>
 
 </html>
