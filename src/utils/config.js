39c2553f33c95454d75816631c2a47c65754afd4
chenminliang01 <chenminliang01@baidu.com>
2017-10-24 17:07:14 +0800

    线上地址

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,13 @@ export const HOST = SERVER_HOST;
 // export const API_PREFIX = `${SERVER_HOST}/api/v1`
 // export const API_PREFIX = 'https://api.github.com';
 // export const API_PREFIX = 'https://www.easy-mock.com/mock/59ca36bee0dc663341bbe24a/smart';
-export const API_PREFIX = 'http://alpha.api.volunteer.tmallwo.com/api/v1';
+
+// export const API_PREFIX = 'http://alpha.api.volunteer.tmallwo.com/api/v1';
+export const API_PREFIX = 'http://api.volunteerzhiduoxing.com';
 
 // 通用接口
-export const API_HOST = window.apiHost || 'http://alpha.api.volunteer.tmallwo.com';
+// export const API_HOST = window.apiHost || 'http://alpha.api.volunteer.tmallwo.com';
+export const API_HOST = window.apiHost || 'http://api.volunteerzhiduoxing.com';
+
+// export const ADMIN_HOST = 'http://alpha.api.admin.volunteer.tmallwo.com';
+export const ADMIN_HOST = 'http://admin.volunteerzhiduoxing.com';
