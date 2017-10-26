38fe94b229c0db5df55c92fda3c6ba6b3683f27c
chenminliang01 <chenminliang01@baidu.com>
2017-10-26 23:12:40 +0800

    android ios 互相跳转问题

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -69,7 +69,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.cd1d598ebc87295fe0dd.js"></script>
+  <script src="/dist/main.100014d8c29884905d25.js"></script>
 </body>
 
 </html>
