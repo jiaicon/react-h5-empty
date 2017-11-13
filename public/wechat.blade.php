408b8fcdf15a8cafed2e23a5f8500a6061780fc0
whq361 <whq361@qq.com>
2017-11-13 14:12:46 +0800

    go back initial verison

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -69,7 +69,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.8f34189ee617ebfd9af4.js"></script>
+  <script src="/dist/main.d492622c41580b830ebb.js"></script>
 </body>
 
 </html>
