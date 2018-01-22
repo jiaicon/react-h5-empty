bd7ef49a60b1bcb569721c074f964a97a91cbf3a
whq361 <whq361@qq.com>
2018-01-22 10:49:17 +0800

    save

--- src/components/community_item/index.js
+++ src/components/community_item/index.js
@@ -189,19 +189,19 @@ class COMMUNITYITEM extends React.Component {
               </Link>
               <ul className="components-community-item-photo-area">
                 {
-              data.photo.length && data.photo.length >= 1 ?
-              data.photo.length === 1 ?
-                <li className="components-community-item-photo-area-li-single" data-index={0} onClick={this.onPreview}>
-                  <IMAGE src={data.photo[0]} className="components-community-item-photo-area-single" />
-                </li> :
-                data.photo.map((itm, index) => (
+                  data.photo && data.photo.length && data.photo.length >= 1 ?
+                data.photo.length === 1 ?
+                  <li className="components-community-item-photo-area-li-single" data-index={0} onClick={this.onPreview}>
+                    <IMAGE src={data.photo[0]} className="components-community-item-photo-area-single" />
+                  </li> :
+                  data.photo.map((itm, index) => (
 
-                  <li className="components-community-item-photo-area-li-several" data-index={index} onClick={this.onPreview}>
-                    <IMAGE src={itm} className="components-community-item-photo-area-several" />
-                  </li>
-                ))
-            : null
-              }
+                    <li className="components-community-item-photo-area-li-several" data-index={index} onClick={this.onPreview}>
+                      <IMAGE src={itm} className="components-community-item-photo-area-several" />
+                    </li>
+                  ))
+              : null
+                }
               </ul>
               {
                 this.props.isDetailEntry && data.team_info && data.team_info.name ? <Link to={`/team/detail/${data.team_info.id}`}><div className="components-community-item-business-container"># {data.team_info.name }</div></Link> : null
@@ -209,7 +209,7 @@ class COMMUNITYITEM extends React.Component {
 
 
               {
-                this.props.isDetailEntry && data.project_info && data.project_info.name ? <Link to={`/project/detail/${data.project_info.id}`}><div className="components-community-item-business-container"># {data.project_info.name }</div>                                                                                                                                                                                                                                                                                                                                                                                       </Link> : null
+                this.props.isDetailEntry && data.project_info && data.project_info.name ? <Link to={`/project/detail/${data.project_info.id}`}><div className="components-community-item-business-container"># {data.project_info.name }</div>                                                                                                                                                                                                                                                                                                                                                                                                 </Link> : null
               }
 
 
