7f0106c5ad5fd34f877f7312d22379b0f49d5fe1
whq361@qq.com <whq361@qq.com>
2018-04-11 23:49:54 +0800

    save

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Wed Apr 11 2018 23:03:17 GMT+0800 (中国标准时间)">
+  <meta name="last-build-time" content="Wed Apr 11 2018 23:49:34 GMT+0800 (中国标准时间)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
