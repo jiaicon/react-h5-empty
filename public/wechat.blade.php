3740af137f245f2a42eb9c18b6d64af25acf317d
whq361@qq.com <whq361@qq.com>
2018-04-12 20:11:58 +0800

    save

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Mon Apr 09 2018 12:29:33 GMT+0800 (中国标准时间)">
+  <meta name="last-build-time" content="Thu Apr 12 2018 20:11:48 GMT+0800 (中国标准时间)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -29,6 +29,7 @@
     // 微信 config，类型可以是 JS Object 或者 JSON 字符串
     window.wxConfig = {!! $jsConfig !!}; // 或者 '{}'
 
+    window.apiHost='{!! $apiHost !!}';
     // server 端下发 token 以及 uniqueKey
     window.token = '{{$token}}';
 
@@ -84,7 +85,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.c3f1974116ccd7c524c7.js"></script>
+  <script src="/dist/main.440df675d6706961e582.js"></script>
 </body>
 
 </html>
--- src/utils/config.js
+++ src/utils/config.js
@@ -12,6 +12,6 @@ export const HOST = SERVER_HOST;
 // export const ADMIN_HOST = 'http://alpha.api.admin.volunteer.tmallwo.com:8000';
 
 // 线上接口
-export const API_PREFIX = 'http://api.volunteerzhiduoxing.com/api/v1';
-export const API_HOST = window.apiHost || 'http://api.volunteerzhiduoxing.com';
-export const ADMIN_HOST = 'http://admin.volunteerzhiduoxing.com';
+export const API_PREFIX = `${window.apiHost}/api/v1` || 'http://alpha.api.volunteer.tmallwo.com:8000/api/v1';
+export const API_HOST = window.apiHost || 'http://alpha.api.volunteer.tmallwo.com:8000';
+export const ADMIN_HOST = `${window.apiHost}` == 'http://api.volunteerzhiduoxing.com' ? 'http://admin.volunteerzhiduoxing.com' : 'http://alpha.api.admin.volunteer.tmallwo.com:8000';
