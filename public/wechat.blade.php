5162451d867d21535bb974d5888476352dc8613d
jiaicon <jiaicon@163.com>
2018-04-18 10:59:25 +0800

    测试bao

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Wed Apr 18 2018 10:22:52 GMT+0800 (中国标准时间)">
+  <meta name="last-build-time" content="Wed Apr 18 2018 10:58:52 GMT+0800 (中国标准时间)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.8df43341c362b7782d92.js"></script>
+  <script src="/dist/main.7b77c89b58f2ccf1e123.js"></script>
 </body>
 
 </html>
