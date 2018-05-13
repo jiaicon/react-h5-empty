565d617026f30ac157c9dc4aa24d2d30708f8a54
whq361@qq.com <whq361@qq.com>
2018-05-13 17:55:07 +0800

    文案

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Thu Apr 26 2018 13:54:48 GMT+0800 (CST)">
+  <meta name="last-build-time" content="Sun May 13 2018 17:54:57 GMT+0800 (中国标准时间)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.b9fdd69a55639ee4305b.js"></script>
+  <script src="/dist/main.491f0852ddb74dbede2f.js"></script>
 </body>
 
 </html>
--- src/pages/project/detail/detail.js
+++ src/pages/project/detail/detail.js
@@ -61,7 +61,7 @@ class ProjectDetailPage extends React.Component {
     };
 
     this.dialog = {
-      title: '确认预约挂号',
+      title: '确认退出',
       buttons: [
         {
           type: 'default',
