09f907a031c2cfcce2aa0b415513dd22f6be7c07
icon <jiaicon@163.com>
2018-06-22 14:47:22 +0800

    修改时间不变的bug

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Fri Jun 22 2018 10:49:24 GMT+0800 (CST)">
+  <meta name="last-build-time" content="Fri Jun 22 2018 14:37:53 GMT+0800 (CST)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.7ea72fe0e4b3e9d4eee2.js"></script>
+  <script src="/dist/main.a2e489d05d9e995087ac.js"></script>
 </body>
 
 </html>
