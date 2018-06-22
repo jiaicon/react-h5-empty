738478d966ced6c0be63edaa4a86467251b2d1dd
icon <jiaicon@163.com>
2018-06-22 16:03:37 +0800

    修改个人资料中图片的ui

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Fri Jun 22 2018 15:53:41 GMT+0800 (CST)">
+  <meta name="last-build-time" content="Fri Jun 22 2018 16:01:32 GMT+0800 (CST)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.fa1cca8817aec77902e2.js"></script>
+  <script src="/dist/main.113cd4b9a2391f3280c4.js"></script>
 </body>
 
 </html>
