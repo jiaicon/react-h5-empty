0fd509a274babd32177f762274a4d06f5bbde2d9
whq361 <whq361@qq.com>
2018-07-05 11:35:54 +0800

    修改登录入口页面

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Mon Jul 02 2018 09:45:11 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Thu Jul 05 2018 10:50:26 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.60ecf56060deca70a5d4.js"></script>
+  <script src="/dist/main.26cd80d35d8f8e37df1a.js"></script>
 </body>
 
 </html>
--- src/pages/my/register/entry.js
+++ src/pages/my/register/entry.js
@@ -38,35 +38,24 @@ class Entry extends React.Component {
     return (
       <div className="page-entry">
         <div className="page-entry-person" />
-        <a onClick={this.onLogin}>
-          <div className="page-entry-person-btn" />
-        </a>
+          <a onClick={this.onLogin}>
+            <div className="page-entry-person-btn" />
+          </a>
         <div className="page-line-box">
           <div className="line1px" style={{ width: '56px' }} />
-          <p className="page-fonts">OR</p>
+                <p className="page-fonts">OR</p>
           <div className="line1px" style={{ width: '56px' }} />
         </div>
         <div className="page-entry-team" />
-        <a href={`${ADMIN_HOST}/login/${window.orgCode}`}>
-          <div className="page-entry-team-btn" />
-        </a>
-        <div className="page-entry-bottom">
-          {/* <Link to="/my/register"> */}
-          <Link to="/my/login">
-            <div className="page-entry-bottom-person">
-            个人注册
-
-            </div>
-            <div className="line1px" style={{ width: '100%', background: '#6AC6F8' }} />
-          </Link>
-          <div className="page-entry-bottom-line" />
+        <div className="page-entry-team-box">
+          <a href={`${ADMIN_HOST}/login/${window.orgCode}`}>
+            <div className="page-entry-team-btn" />
+          </a>
           <a href={`${ADMIN_HOST}/register/${window.orgCode}`}>
-            <div className="page-entry-bottom-person">
-            团队注册
-            </div>
-            <div className="line1px" style={{ width: '100%', background: '#6AC6F8' }} />
+            <div className="page-entry-team-btn" />
           </a>
         </div>
+       
       </div>
     );
   }
