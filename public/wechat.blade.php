0ae6171b44f6d293052028a77ac2fc436619661c
whq361 <whq361@qq.com>
2018-08-07 18:58:13 +0800

    修改code ssh

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Wed Aug 01 2018 22:53:59 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Tue Aug 07 2018 18:54:27 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,7 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-  <script src="/dist/main.9b242dd434d351c54c5e.js"></script>
+  <script src="/dist/main.3307179ea4148297e6ed.js"></script>
 </body>
 
 </html>
