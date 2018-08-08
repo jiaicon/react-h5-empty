fa7ec3266d2ecfde6e9d5e68d92ca7467eaeceb8
whq361 <whq361@qq.com>
2018-08-08 10:58:14 +0800

    修改merge

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Tue Aug 07 2018 19:26:22 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Wed Aug 08 2018 10:56:33 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -87,11 +87,7 @@
   <div id="container">
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
-<<<<<<< HEAD
-  <script src="/dist/main.261910f264f775f280d7.js"></script>
-=======
-  <script src="/dist/main.a8964cd0b0f12aea03fe.js"></script>
->>>>>>> fbe3e1d6ebe270e2e435ed661ed46aa3a2307edf
+  <script src="/dist/main.c5bde72f304bece6c7f1.js"></script>
 </body>
 
 </html>
