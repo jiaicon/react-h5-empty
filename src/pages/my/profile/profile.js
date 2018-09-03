08d7022412dff36b1e21c52ce57af60947f1d207
whq361 <whq361@qq.com>
2018-09-03 17:08:23 +0800

    修改个人资料

--- src/pages/my/profile/profile.js
+++ src/pages/my/profile/profile.js
@@ -422,12 +422,11 @@ class Profile extends React.Component {
                     'page-profile-display-extends-none': user.id_number,
                     })}
                     >
-                        {
-                            window.orgInfo.real_name_register?
+                        
                             <Link to="/my/profile/verify">
                                 <div className="page-profile-bottom-btn">申请成为实名注册志愿者</div>
-                            </Link>:null
-                        }
+                            </Link>
+                        
                     </div>
                
               
