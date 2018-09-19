7c3df155e6974c8bbe2685c3a917c6092a00e2b0
whq361 <whq361@qq.com>
2018-09-19 13:49:58 +0800

    修改ui

--- src/components/shopItme/index.js
+++ src/components/shopItme/index.js
@@ -44,8 +44,10 @@ class Projects extends React.Component {
                                     <div className="component-shopItem-container">
                                         <Image src={item.g_img} className="image" defaultSrc="/images/my/banner.png" />
                                         <div className="component-shopItem-content">
-                                            <div className="component-shopItem-name">{item.g_name}</div>
-                                            <div className="component-shopItem-condition">{item.condition}</div>
+                                            <div className="component-shopItem-name-container">
+                                                <div className="component-shopItem-name">{item.g_name}</div>
+                                                <div className="component-shopItem-condition">{item.condition}</div>
+                                            </div>
                                             <div className="component-shopItem-price-container">
                                                 <div className="new">{item.points}</div>
                                                 <div className="fonts">星币</div>
@@ -64,7 +66,7 @@ class Projects extends React.Component {
             </ul>
         );
     }
-    onSure(id){
+    onSure(id) {
         this.props.isSure(id);
     }
     renderOrderList() {
@@ -77,45 +79,48 @@ class Projects extends React.Component {
         return (
             <ul className="component-shopItem">
                 {
-                 orderData.map((item)=>{
-                     return (
-                        <Link to=''>
-                        <li >
-                            <div className="component-shopItem-container">
-                                <Image src={item && item.goods_id && item.goods_id.thumbnail?item.goods_id.thumbnail:''} className="image" defaultSrc="/images/my/banner.png" />
-                                <div className="component-shopItem-content">
-                                    <div className="component-shopItem-name">{item && item.goods_id && item.goods_id.g_name?item.goods_id.g_name:null}</div>
-                                    <div className="component-shopItem-condition">{item && item.goods_id && item.goods_id.condition?item.goods_id.condition:null} </div>
-                                    <div className="component-shopItem-price-container">
-                                        <div className="new">{item && item.goods_id && item.goods_id.points?item.goods_id.points:null}</div>
-                                        <div className="fonts">星币</div>
-                                        <div className="old">¥{item && item.goods_id && item.goods_id.price?item.goods_id.price:null}元</div>
+                    orderData.map((item) => {
+                        return (
+                            <Link to=''>
+                                <li >
+                                    <div className="component-shopItem-container">
+                                        <Image src={item && item.goods_id && item.goods_id.thumbnail ? item.goods_id.thumbnail : ''} className="image" defaultSrc="/images/my/banner.png" />
+                                        <div className="component-shopItem-content">
+                                            <div className="component-shopItem-name-container">
+                                                <div className="component-shopItem-name">{item && item.goods_id && item.goods_id.g_name ? item.goods_id.g_name : null}</div>
+                                                <div className="component-shopItem-condition">{item && item.goods_id && item.goods_id.condition ? item.goods_id.condition : null} </div>
+                                            </div>
+
+                                            <div className="component-shopItem-price-container">
+                                                <div className="new">{item && item.goods_id && item.goods_id.points ? item.goods_id.points : null}</div>
+                                                <div className="fonts">星币</div>
+                                                <div className="old">¥{item && item.goods_id && item.goods_id.price ? item.goods_id.price : null}元</div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="line1px" />
-                            <div className="component-shopItem-order-container">
-                                <div className="component-shopItem-order-time">下单时间：{item.updated_at}</div>
-                                {
-                                    item.state ?
-                                        <div className="component-shopItem-order-time">已兑换／发货时间：{item.collect_time}</div> : null}
-                                {
-    
-                                    !item.state && item.status == 0 ? <div className="component-shopItem-order-btn" onClick={()=>this.onSure(item.id)}>确认已兑换</div> : null}
-                                {
-                                    !item.state && item.status == 1 ? <div className="component-shopItem-order-time">兑换失败：{item.reason}</div> : null}
-                                {
-                                    !item.state && item.status == 2 ? <div className="component-shopItem-order-time component-shopItem-order-time-fonts-color">审核中</div> : null
-                                }
-    
-                            </div>
-    
-    
-                        </li>
-                        <div className="takeupborder" />
-                    </Link>
-                     )
-                 })
+                                    <div className="line1px" />
+                                    <div className="component-shopItem-order-container">
+                                        <div className="component-shopItem-order-time">下单时间：{item.updated_at}</div>
+                                        {
+                                            item.state ?
+                                                <div className="component-shopItem-order-time">已兑换／发货时间：{item.collect_time}</div> : null}
+                                        {
+
+                                            !item.state && item.status == 0 ? <div className="component-shopItem-order-btn" onClick={() => this.onSure(item.id)}>确认已兑换</div> : null}
+                                        {
+                                            !item.state && item.status == 1 ? <div className="component-shopItem-order-time">兑换失败：{item.reason}</div> : null}
+                                        {
+                                            !item.state && item.status == 2 ? <div className="component-shopItem-order-time component-shopItem-order-time-fonts-color">审核中</div> : null
+                                        }
+
+                                    </div>
+
+
+                                </li>
+                                <div className="takeupborder" />
+                            </Link>
+                        )
+                    })
                 }
             </ul>
         )
