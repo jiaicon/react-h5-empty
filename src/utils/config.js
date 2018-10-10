aba3cba952f03b8db641f208bcd07370bf3bc8c5
whq361 <whq361@qq.com>
2018-10-10 17:20:01 +0800

    修改团队登录入口

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,5 +5,5 @@ export const HOST = SERVER_HOST;
 // 线上接口
 export const API_PREFIX = `${window.apiHost}/api/v1` || 'http://alpha.api.volunteer.tmallwo.com:8000/api/v1';
 export const API_HOST = window.apiHost || 'http://alpha.api.volunteer.tmallwo.com:8000';
-export const ADMIN_HOST = `${window.apiHost}` == 'http://api.volzdx.cn' ? 'http://admin.volzdx.cn' : 'http://alpha.api.admin.volunteer.tmallwo.com:8000';
+export const ADMIN_HOST = `${window.apiHost}` == 'http://api.volzdx.cn' ? 'http://admin.volzdx.cn' : 'http://api.admin.volunteer.alpha.flashdiet.cn';
 // http://api.volzdx.cn
