ce33516cc62e37e9eb452e8e0457c9a1f60fe9af
whq361 <whq361@whq361deMacBook-Pro.local>
2018-12-10 20:06:25 +0800

    test测试沈阳

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -106,6 +106,7 @@ class HomePage extends React.Component {
 
       getCity(
         city => {
+          console.log(city)
           this.setState({ ...this.state, showDialog: true, city });
         },
         () => {
