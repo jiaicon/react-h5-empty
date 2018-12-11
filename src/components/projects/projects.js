1e627b89fe90b38287cb2e36b02429ce7f0c99a8
whq361 <whq361@whq361deMBP.lan>
2018-12-11 23:09:59 +0800

    修改done

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -45,61 +45,58 @@ class Projects extends React.Component {
           projects.map((project) => {
             const { team } = project;
             const volunteer = isVolunteerInsure(project.volunteer_security);
-            return (<li key={project.id}>
-              <div>
-                <Link to={`/team/detail/${project.team.id}`} className="project-header">
-                  <Avatar src={team.logo} size={{ width: 30, radius: 4 }} />
-                  <div className="org-name">{team.name}</div>
-                </Link>
-                <Link to={`/project/detail/${project.id}`} className="project-main">
-                  <Image
-                    className="image"
-                    src={project.list_photo}
-                    defaultSrc="/images/default_banner.png" alt="项目图片"
-                  />
-                  <div className="project-name">
-                    {project.name}
+            return <li key={project.id}>
+                <div>
+                  <Link to={`/team/detail/${project.team.id}`} className="project-header">
+                    <Avatar src={team.logo} size={{ width: 30, radius: 4 }} />
+                    <div className="org-name">{team.name}</div>
+                  </Link>
+                  <Link to={`/project/detail/${project.id}`} className="project-main">
+                    <Image className="image" src={project.list_photo} defaultSrc="/images/default_banner.png" alt="项目图片" />
+                    <div className="project-name">
+                      {project.name}
 
-                    <div
-                      className={classnames({
-                        'project-name-logo': volunteer,
-                      })}
-                    />
-                  </div>
-                  <div className="project-date">活动日期：{parseTimeStringToDateString(project.begin)}-{parseTimeStringToDateString(project.end)}</div>
-                  {
-                    showLabel ?
-                      <div
-                        className={classnames({
-                          'project-status': true,
-                          'project-status-employed': project.join_status === 1 && project.project_status !== 5,
-                          'project-status-employed-wait': project.join_status === 0 && project.project_status !== 5,
-                          'project-status-employed-end': project.project_status === 5,
-                        })}
-                      />
-                    :
-                      <div
-                        className={classnames({
-                          'project-status': true,
-                          'project-status-recruit': project.project_status === 2,
-                          'project-status-full': project.project_status === 3,
-                          'project-status-progress': project.project_status === 4,
-                          'project-status-end': project.project_status === 5,
-
-                        })}
-                      />
-                  }
-                </Link>
-                <div className="project-footer">
-                  <div className="project-location">{project.county_name} {parseDistance(project.distance)}</div>
-                  <div className="project-members">
-                    <span>{project.join_people_count}</span>
-                    &nbsp;/&nbsp;
-                    <span>{project.people_count}</span>
+                      <div className={classnames({
+                          "project-name-logo": volunteer
+                        })} />
+                    </div>
+                    <div className="project-date">
+                      活动日期：{parseTimeStringToDateString(project.begin)}-{parseTimeStringToDateString(project.end)}
+                    </div>
+                    {showLabel ? <div className={classnames({
+                          "project-status": true,
+                          "project-status-employed":
+                            project.join_status === 1 &&
+                            project.project_status !== 5,
+                          "project-status-employed-wait":
+                            project.join_status === 0 &&
+                            project.project_status !== 5,
+                          "project-status-employed-end":
+                            project.project_status === 5
+                        })} /> : <div className={classnames({
+                          "project-status": true,
+                          "project-status-recruit":
+                            project.project_status === 2,
+                          "project-status-full":
+                            project.project_status === 3,
+                          "project-status-progress":
+                            project.project_status === 4,
+                          "project-status-end":
+                            project.project_status === 5
+                        })} />}
+                  </Link>
+                  <div className="project-footer">
+                    <div className="project-location">
+                      {project.county_name} {project.distance>0?parseDistance(project.distance):null}
+                    </div>
+                    <div className="project-members">
+                      <span>{project.join_people_count}</span>
+                      &nbsp;/&nbsp;
+                      <span>{project.people_count}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>);
+              </li>;
           })
         }
       </ul>
