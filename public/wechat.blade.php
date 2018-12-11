e5bbf9fe2a2cad94024d3e3c82d2e67315f5f827
whq361 <whq361@whq361deMacBook-Pro.local>
2018-12-11 10:46:59 +0800

    getlocation test script tag2"

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Tue Dec 11 2018 10:38:33 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Tue Dec 11 2018 10:43:54 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -88,7 +88,7 @@
   </div>
   <!-- async="async" defer="defer" -->
   <!-- <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp&key=GT7BZ-UXACR-R2JWZ-WYSXR-DHWJV-VEFAI"></script> -->
-  <script src="/dist/main.51c4f8f63d9b12ec2f41.js"></script>
+  <script src="/dist/main.3de913db969a0785e33e.js"></script>
 </body>
 
 </html>
