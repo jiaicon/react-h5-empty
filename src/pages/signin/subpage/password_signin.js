d0bd10245d4387d7ebc95f0151e70df4815eea00
whq361 <whq361@qq.com>
2019-01-21 13:45:18 +0800

    修改密码框ui

--- src/pages/signin/subpage/password_signin.js
+++ src/pages/signin/subpage/password_signin.js
@@ -1,36 +1,33 @@
 /* global wx:false */
 /* eslint  "jsx-a11y/no-static-element-interactions":"off", "class-methods-use-this":"off" */
 
-import React, { PropTypes } from 'react';
-import autoBind from 'react-autobind';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import Alert from 'react-s-alert';
-import WXShare from '../../../components/share';
-import ReactCodeInput from '../../../components/code_input/ReactCodeInput';
-import { checkin } from '../../signin/signin.store';
-import './password_signin.css';
-import history from '../../history';
-import { getCity, getLocation } from '../../../utils/funcs';
-import { requestHomeData, saveCity, getAreaCity } from '../../home/home.store';
-
+import React, { PropTypes } from "react";
+import autoBind from "react-autobind";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import Alert from "react-s-alert";
+import WXShare from "../../../components/share";
+import ReactCodeInput from "../../../components/code_input/ReactCodeInput";
+import { checkin } from "../../signin/signin.store";
+import "./password_signin.css";
+import history from "../../history";
+import { getCity, getLocation } from "../../../utils/funcs";
+import { requestHomeData, saveCity, getAreaCity } from "../../home/home.store";
 
 class PasswordSigninPage extends React.Component {
-
   constructor(props) {
     super(props);
     autoBind(this);
   }
 
-  componentWillMount() {
-  }
+  componentWillMount() {}
 
   componentDidMount() {
-      if(window.userAgent) {
-          wx.ready(() => {
-              WXShare();
-          });
-      }
+    if (window.userAgent) {
+      wx.ready(() => {
+        WXShare();
+      });
+    }
   }
 
   componentWillReceiveProps(nextProps) {
@@ -38,7 +35,7 @@ class PasswordSigninPage extends React.Component {
     const { checkinData: Ncheckin } = nextProps;
     if (Lcheckin.fetching && !Ncheckin.fetching && !Ncheckin.failed) {
       // window.location.replace('/signin');
-      history.replace('/signin');
+      history.replace("/signin");
     }
   }
 
@@ -46,7 +43,7 @@ class PasswordSigninPage extends React.Component {
   onUserInput(e) {
     const key = e;
     this.setState({
-      key,
+      key
     });
     if (key.length === 6) {
       this.props.checkin(key, 2);
@@ -55,57 +52,63 @@ class PasswordSigninPage extends React.Component {
   render() {
     return (
       <div className="page-password-container">
-        <div className="page-password-title">输入团队提供的数字，即可完成签到</div>
-        <ReactCodeInput
-          onChange={this.onUserInput}
-          value=""
-          isValid
-          fields={6}
-          type="number"
-          disabled={false}
-          inputStyle={{
-            borderBottom: '1px solid #E5E5E5',
-            marginRight: '10px',
-            paddingLeft: '6px',
-            paddingRight: '6px',
-            paddingTop: '15px',
-            paddingBottom: '15px',
-            textAlign: 'center',
-            fontSize: '24px',
-            boxSizing: 'border-box',
-            color: 'black',
-            backgroundColor: 'white',
-            borderColor: 'lightgrey' }}
-        />
+        <div className="page-password-title">
+          输入团队提供的数字，即可完成签到
+        </div>
+    
+          <ReactCodeInput
+            onChange={this.onUserInput}
+            value=""
+            isValid
+            fields={6}
+            type="number"
+            disabled={false}
+            inputStyle={{
+              borderBottom: "1px solid #E5E5E5",
+              marginRight: "10px",
+              width:'50px',
+              height: '54px',
+              textAlign: "center",
+              fontSize: "24px",
+              boxSizing: "border-box",
+              color: "black",
+              backgroundColor: "white",
+              borderColor: "lightgrey"
+            }}
+          />
       </div>
     );
   }
 }
 
-PasswordSigninPage.title = '密令打卡';
+PasswordSigninPage.title = "密令打卡";
 
 PasswordSigninPage.propTypes = {
   data: PropTypes.shape({
     list: PropTypes.arrayOf(PropTypes.shape({})),
-    next: PropTypes.shape({}),
+    next: PropTypes.shape({})
   }),
   checkinData: PropTypes.shape({
     fetching: PropTypes.bool,
-    failed: PropTypes.bool,
+    failed: PropTypes.bool
   }),
   signin: PropTypes.shape({
     list: PropTypes.arrayOf(PropTypes.shape({})),
     next: PropTypes.shape({}),
     fetching: PropTypes.bool,
-    failed: PropTypes.bool,
+    failed: PropTypes.bool
   }),
   checkin: PropTypes.func,
-  requestCheckinList: PropTypes.func,
+  requestCheckinList: PropTypes.func
 };
 
 export default connect(
   state => ({
-    checkinData: state.signin.checkin,
+    checkinData: state.signin.checkin
   }),
-  dispatch => bindActionCreators({ checkin, requestHomeData, saveCity, getAreaCity }, dispatch),
+  dispatch =>
+    bindActionCreators(
+      { checkin, requestHomeData, saveCity, getAreaCity },
+      dispatch
+    )
 )(PasswordSigninPage);
