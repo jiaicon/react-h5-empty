07ede39858d10be81e656b3bd88cc4522650914a
andy136240860 <739917432@qq.com>
2019-02-15 19:32:33 +0800

    list列表

--- src/pages/my/fundingApplication/list.js
+++ src/pages/my/fundingApplication/list.js
@@ -1,163 +1,164 @@
-import React, { PropTypes } from 'react';
-import autoBind from 'react-autobind';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { projectAction } from '../my.store';
 
-import Link from '../../../components/link/link';
+// import React, { PropTypes } from 'react';
+// import autoBind from 'react-autobind';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import { projectAction } from '../my.store';
 
-import classnames from 'classnames';
-import AllPage from './projects_all';
-import WaitPage from './projects_wait';
-import PassPage from './projects_pass';
-import EndPage from './projects_end';
+// import Link from '../../../components/link/link';
 
-import './projects.css';
+// import classnames from 'classnames';
+// import AllPage from './projects_all';
+// import WaitPage from './projects_wait';
+// import PassPage from './projects_pass';
+// import EndPage from './projects_end';
 
-const TAB_URL_MAPS = {
-  '/my/projects/': <AllPage />,
-  '/my/projects/wait': <WaitPage />,
-  '/my/projects/pass': <PassPage />,
-  '/my/projects/end': <EndPage />,
-};
-class Projects extends React.Component {
+// import './projects.css';
 
-  constructor(props) {
-    super(props);
-    autoBind(this);
-    this.state = {
-      page: this.getTabName(this.props),
-    };
-  }
+// const TAB_URL_MAPS = {
+//   '/my/projects/': <AllPage />,
+//   '/my/projects/wait': <WaitPage />,
+//   '/my/projects/pass': <PassPage />,
+//   '/my/projects/end': <EndPage />,
+// };
+// class Projects extends React.Component {
 
-  componentWillMount() {
-    // this.props.projectAction();
-  }
+//   constructor(props) {
+//     super(props);
+//     autoBind(this);
+//     this.state = {
+//       page: this.getTabName(this.props),
+//     };
+//   }
 
-  componentDidMount() {
+//   componentWillMount() {
+//     // this.props.projectAction();
+//   }
 
-  }
+//   componentDidMount() {
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      ...this.state,
-      page: this.getTabName(nextProps),
-    });
-  }
+//   }
 
-  componentWillUnmount() {}
-  getTabName(props) {
-    return TAB_URL_MAPS[(props || this.props).route.path];
-  }
-  render() {
-    const { page } = this.state;
-    const { path } = this.props.route;
-    return (
-      <div className="page-projects-container">
-        <div style={{ width: '100%', height: '50px' }}>
-          <ul className="page-projects-tab-container">
-            <li>
-              <Link to="/my/projects/">
-                <div
-                  className={classnames({
-                    'page-projects-li-a-div-style': true,
-                    active: path === '/my/projects/',
-                  })}
-                >全部</div>
-              </Link>
-            </li>
-            <li>
-              <Link to="/my/projects/wait">
-                <div
-                  className={classnames({
-                    'page-projects-li-a-div-style': true,
-                    active: path === '/my/projects/wait',
-                  })}
-                >待录用</div>
-              </Link>
-            </li>
-            <li>
-              <Link to="/my/projects/pass">
-                <div
-                  className={classnames({
-                    'page-projects-li-a-div-style': true,
-                    active: path === '/my/projects/pass',
-                  })}
-                >已录用</div>
-              </Link>
-            </li>
-            <li>
-              <Link to="/my/projects/end">
-                <div
-                  className={classnames({
-                    'page-projects-li-a-div-style': true,
-                    active: path === '/my/projects/end',
-                  })}
-                >已结束</div>
-              </Link>
-            </li>
-          </ul>
-          <div className="line1px" style={{ width: '100%' }} />
-        </div>
-        <div className="page-projects-content-main">
-          {page}
-        </div>
+//   componentWillReceiveProps(nextProps) {
+//     this.setState({
+//       ...this.state,
+//       page: this.getTabName(nextProps),
+//     });
+//   }
 
-      </div>
-    );
-  }
-}
+//   componentWillUnmount() {}
+//   getTabName(props) {
+//     return TAB_URL_MAPS[(props || this.props).route.path];
+//   }
+//   render() {
+//     const { page } = this.state;
+//     const { path } = this.props.route;
+//     return (
+//       <div className="page-projects-container">
+//         <div style={{ width: '100%', height: '50px' }}>
+//           <ul className="page-projects-tab-container">
+//             <li>
+//               <Link to="/my/projects/">
+//                 <div
+//                   className={classnames({
+//                     'page-projects-li-a-div-style': true,
+//                     active: path === '/my/projects/',
+//                   })}
+//                 >全部</div>
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/my/projects/wait">
+//                 <div
+//                   className={classnames({
+//                     'page-projects-li-a-div-style': true,
+//                     active: path === '/my/projects/wait',
+//                   })}
+//                 >待录用</div>
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/my/projects/pass">
+//                 <div
+//                   className={classnames({
+//                     'page-projects-li-a-div-style': true,
+//                     active: path === '/my/projects/pass',
+//                   })}
+//                 >已录用</div>
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/my/projects/end">
+//                 <div
+//                   className={classnames({
+//                     'page-projects-li-a-div-style': true,
+//                     active: path === '/my/projects/end',
+//                   })}
+//                 >已结束</div>
+//               </Link>
+//             </li>
+//           </ul>
+//           <div className="line1px" style={{ width: '100%' }} />
+//         </div>
+//         <div className="page-projects-content-main">
+//           {page}
+//         </div>
+
+//       </div>
+//     );
+//   }
+// }
 
 
-Projects.title = '志愿项目';
+// Projects.title = '志愿项目';
 
-Projects.propTypes = {
-  projectAction: PropTypes.func,
-  project: PropTypes.shape({
-    data: PropTypes.shape({
-      list: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.number,
-          name: PropTypes.string,
-          photo: PropTypes.array,
-          content: PropTypes.string,
-          identifier: PropTypes.string,
-          province_id: PropTypes.number,
-          province_name: PropTypes.string,
-          city_id: PropTypes.number,
-          city_name: PropTypes.string,
-          county_id: PropTypes.number,
-          county_name: PropTypes.string,
-          addr: PropTypes.string,
-          lng: PropTypes.string,
-          lat: PropTypes.string,
-          join_begin: PropTypes.string,
-          join_end: PropTypes.string,
-          begin: PropTypes.string,
-          end: PropTypes.string,
-          people_count: PropTypes.number,
-          join_people_count: PropTypes.number,
-          reward_time: PropTypes.string,
-          activity_status: PropTypes.number,
-          category: PropTypes.arrayOf(PropTypes.shape({})),
-          service_object: PropTypes.arrayOf(PropTypes.shape({})),
-          team: PropTypes.shape({}),
-          join_status: PropTypes.number,
-          my_reward_time: PropTypes.number,
-        }),
-      ),
-    }),
-  }),
-  route: PropTypes.shape({
-    path: PropTypes.string,
-  }),
-};
+// Projects.propTypes = {
+//   projectAction: PropTypes.func,
+//   project: PropTypes.shape({
+//     data: PropTypes.shape({
+//       list: PropTypes.arrayOf(
+//         PropTypes.shape({
+//           id: PropTypes.number,
+//           name: PropTypes.string,
+//           photo: PropTypes.array,
+//           content: PropTypes.string,
+//           identifier: PropTypes.string,
+//           province_id: PropTypes.number,
+//           province_name: PropTypes.string,
+//           city_id: PropTypes.number,
+//           city_name: PropTypes.string,
+//           county_id: PropTypes.number,
+//           county_name: PropTypes.string,
+//           addr: PropTypes.string,
+//           lng: PropTypes.string,
+//           lat: PropTypes.string,
+//           join_begin: PropTypes.string,
+//           join_end: PropTypes.string,
+//           begin: PropTypes.string,
+//           end: PropTypes.string,
+//           people_count: PropTypes.number,
+//           join_people_count: PropTypes.number,
+//           reward_time: PropTypes.string,
+//           activity_status: PropTypes.number,
+//           category: PropTypes.arrayOf(PropTypes.shape({})),
+//           service_object: PropTypes.arrayOf(PropTypes.shape({})),
+//           team: PropTypes.shape({}),
+//           join_status: PropTypes.number,
+//           my_reward_time: PropTypes.number,
+//         }),
+//       ),
+//     }),
+//   }),
+//   route: PropTypes.shape({
+//     path: PropTypes.string,
+//   }),
+// };
 
-export default connect(
-  state => ({
-    project: state.my.project,
-  }),
-  dispatch => bindActionCreators({
-    projectAction,
-  }, dispatch),
-)(Projects);
+// export default connect(
+//   state => ({
+//     project: state.my.project,
+//   }),
+//   dispatch => bindActionCreators({
+//     projectAction,
+//   }, dispatch),
+// )(Projects);
