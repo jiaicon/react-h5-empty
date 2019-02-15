0275828002686ee0d94ca61901fd0135d051c882
andy136240860 <739917432@qq.com>
2019-02-15 14:21:17 +0800

    更改测试服务器上传脚本

--- tools/deploy-testserver.sh
+++ tools/deploy-testserver.sh
@@ -1,5 +1,5 @@
 git stash
 git pull
 yarn build
-scp -r  ./public/wechat.blade.php miller@39.107.251.62:/data/webapp/volunteer/Volunteer_wechat/resources/views/wechat.blade.php
-scp -r  ./public/* miller@39.107.251.62:/data/webapp/volunteer/Volunteer_wechat/public/
+scp -r  ./public/wechat.blade.php root@39.107.251.62:/data/webapp/volunteer/Volunteer_wechat/resources/views/wechat.blade.php
+scp -r  ./public/* root@39.107.251.62:/data/webapp/volunteer/Volunteer_wechat/public/
