d38d61a77553a27aabeabcf7f999092a4b226c82
andy136240860 <739917432@qq.com>
2019-02-24 21:51:21 +0800

    修改星巴克登录图片

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Sun Feb 24 2019 20:46:36 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Sun Feb 24 2019 21:05:35 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -92,7 +92,7 @@
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp&key=GT7BZ-UXACR-R2JWZ-WYSXR-DHWJV-VEFAI"></script>
   <script type="text/javascript" src="https://3gimg.qq.com/lightmap/components/geolocation/geolocation.min.js"></script>
-  <script src="/dist/main.a9ddd06c372e4070ac1e.js"></script>
+  <script src="/dist/main.f3529819acb5c9c7ed84.js"></script>
 </body>
 
 </html>
