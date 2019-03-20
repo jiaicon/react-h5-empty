bbc7de22a66bee925f50d677dc1df6d447317bf4
jiaicon@163.com <jiaicon@163.com>
2019-03-20 14:22:17 +0800

    志愿成就里的UI样式

--- src/pages/my/achievemet/achievemet.js
+++ src/pages/my/achievemet/achievemet.js
@@ -111,28 +111,33 @@ class Achievement extends React.Component {
             }, 0);
         }
     }
-
     openModalAchievement(data) {
-        console.log(`${window.location.protocol}//${window.location.host}`)
+        console.log("打开的成就信息：：", data)
         const {user} = this.props;
         let reward_time = user.reward_time && Number(user.reward_time).toFixed(0);
         let modalMod = data ? <div className="achievement-modal-box">
             <div className={classnames({
                 'achievement-modal-box-all': true,
-                'achievement-modal-box-all-gold': data.level === 3,
+                'achievement-modal-box-all-gold': data.level === 3 || !data.level,
                 'achievement-modal-box-all-silver': data.level === 2,
                 'achievement-modal-box-all-iron': data.level === 1,
             })}>
                 <div className="achievement-modal-box-all-box">
                     <div
-                        style={{backgroundImage: `url(${data.icon})`}}></div>
-                    <p>{data.name}</p>
+                        style={{backgroundImage: `url(${data.icon || data.achieve_info[0].icon})`}}></div>
+                    <p>{data.achieve_info && data.achieve_info.length && data.achieve_info[0].name}</p>
                 </div>
             </div>
             {
                 data.achieve_info && data.achieve_info.length && data.achieve_info[0].settings ?
                     <div>
-                        <div className="achievement-modal-box-level achievement-modal-box-level-gold"></div>
+                        <div className={classnames({
+                            "achievement-modal-box-level": true,
+                            "achievement-modal-box-level-gold": data.level === 3,
+                            "achievement-modal-box-level-silver": data.level === 2,
+                            "achievement-modal-box-level-iron": data.level === 1,
+                            "achievement-modal-box-level-default": !data.level,
+                        })}></div>
                         <div className="achievement-modal-box-level-box">
                             <div>
                                 <p>达到{data.achieve_info[0].settings.achieve1}小时</p>
@@ -156,11 +161,13 @@ class Achievement extends React.Component {
                 <div className={classnames({
                     'achievement-modal-box-btn-default': true,
                     'achievement-modal-box-btn-left': true,
+                    'achievement-modal-box-btn-left-hidden': !data.name,
                     'achievement-modal-box-btn-left-flex1': data.level === 3,
                 })} onClick={this.modalThis2Img}>告诉大家</div>
                 <Link to="/project/list" className={classnames({
                     'achievement-modal-box-btn-default': true,
                     'achievement-modal-box-btn-right': true,
+                    'achievement-modal-box-btn-left-flex1': !data.name,
                     'hidden': data.level === 3
                 })}>继续做志愿</Link>
             </div>
@@ -264,7 +271,8 @@ class Achievement extends React.Component {
                     {
                         achievementList.data && achievementList.data.list && achievementList.data.list.length > 0 && achievementList.data.list.map((item, index) => (
                             <div className="achievement-array-item"
-                                 data-index={item.achieve_key}>
+                                 data-index={item.achieve_key}
+                                 onClick={this.getAchieveInfo}>
                                 <div style={{backgroundImage: `url(${item.icon})`}}></div>
                                 <p>{item.name}</p>
                             </div>
