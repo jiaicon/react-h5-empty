3845a61daad4efe09fad3d0080f711ed61a2df6d
jiaicon@163.com <jiaicon@163.com>
2019-03-22 15:35:07 +0800

    提交代码

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Thu Mar 21 2019 14:17:37 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Fri Mar 22 2019 14:57:14 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -132,7 +132,7 @@
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp&key=GT7BZ-UXACR-R2JWZ-WYSXR-DHWJV-VEFAI"></script>
   <script type="text/javascript" src="https://3gimg.qq.com/lightmap/components/geolocation/geolocation.min.js"></script>
-  <script src="/dist/main.859a6d184629555016d3.js"></script>
+  <script src="/dist/main.8d9490a9f7e80631dd5b.js"></script>
 </body>
 
 </html>
