8a877cb8e4ab3e8782a84f7534a04ffa2d380fb0
jiaicon@163.com <jiaicon@163.com>
2019-03-23 17:17:22 +0800

    成就tips

--- src/pages/my/achievemet/achievement.store.js
+++ src/pages/my/achievemet/achievement.store.js
@@ -107,8 +107,45 @@ const getAchieveInfoRedux = (state={
             return state;
     }
 };
+
+//分享成就
+export const getAchieveShareInfo = (key) => ({
+    type: 'GET_ACHIEVE_SHARE_INFO',
+    payload: fetch(`/user/achievement/${key}/share`, {method: 'GET'})
+});
+const getAchieveInfoShareRedux = (state={
+    fetching: false,
+    failed: false,
+    data: null,
+}, action)=>{
+    switch (action.type) {
+        case 'GET_ACHIEVE_SHARE_INFO_PENDING':
+            return {
+                ...state,
+                fetching: true,
+                failed: false
+            };
+        case 'GET_ACHIEVE_SHARE_INFO_FULFILLED':
+            return {
+                ...state,
+                fetching: false,
+                failed: false,
+                data: action.payload&&action.payload.data
+            };
+        case 'GET_ACHIEVE_SHARE_INFO_REJECTED':
+            return {
+                ...state,
+                fetching: false,
+                failed: true,
+                data: action.payload
+            };
+        default:
+            return state;
+    }
+};
 const reducer =  combineReducers({
     getAchieveInfo: getAchieveInfoRedux,
+    getAchieveInfoShare: getAchieveInfoShareRedux,
     achievementList: getAchievementRedux,
     achievementHasList: getAchievementHasRedux
 });
