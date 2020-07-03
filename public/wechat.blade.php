bc35cab81dc6ef3ba652d09cf35ed0f77b928e84
andy136240860 <739917432@qq.com>
2020-07-03 18:49:08 +0800

    fixbugs

--- public/wechat.blade.php
+++ public/wechat.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="x5-orientation"content="portrait">
-  <meta name="last-build-time" content="Fri Jul 03 2020 18:28:35 GMT+0800 (GMT+08:00)">
+  <meta name="last-build-time" content="Fri Jul 03 2020 18:45:40 GMT+0800 (GMT+08:00)">
   <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
   <script>
     var designWidth = 375;
@@ -132,7 +132,7 @@
   </div>
   <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp&key=GT7BZ-UXACR-R2JWZ-WYSXR-DHWJV-VEFAI"></script>
   <script type="text/javascript" src="https://3gimg.qq.com/lightmap/components/geolocation/geolocation.min.js"></script>
-  <script src="/dist/main.4d4580bdb2ff789cd11e.js"></script>
+  <script src="/dist/main.95c429ac3acae28868db.js"></script>
 </body>
 
 </html>
