f3168b07ab4122240bbeab078e67a19b5942481f
andy136240860 <739917432@qq.com>
2020-08-02 23:23:24 +0800

    更改测试环境跳转链接

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ export const HOST = SERVER_HOST;
 // 线上接口
 export const API_PREFIX = `${window.apiHost}/api/v1` || 'http://alpha.api.volunteer.tmallwo.com:8000/api/v1';
 export const API_HOST = window.apiHost || 'http://alpha.api.volunteer.tmallwo.com:8000';
-export const ADMIN_HOST = (`${window.apiHost}` == 'https://api.volzdx.cn') || (`${window.apiHost}` == 'https://api.volzdx.cn' )? 'https://admin.volzdx.cn' : 'https://api.volunteer.alpha.guangying.link';
+export const ADMIN_HOST = (`${window.apiHost}` == 'https://api.volzdx.cn') || (`${window.apiHost}` == 'https://api.volzdx.cn' )? 'https://admin.volzdx.cn' : 'http://admin.volunteer.guangying.link';
 // http://api.volzdx.cn
 
 
